78a238fa96afac249ce15e03b6c8b75091bda584
Ryong-E <think8867@gmail.com>
2023-03-07 20:47:34 +0900

    [#1] feature: CRA 환경 셋팅
    prettier,eslint 적용
    husky 설치 npm install시 자동 설치
    commit시 prettier적용 push시 lint적용

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePattern: ['.eslintrc.js', 'public', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
+    eqeqeq: 'error', // 일치 연산자 사용 필수
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
+    'no-unused-vars': 'error', // 사용하지 않는 변수 금지
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
