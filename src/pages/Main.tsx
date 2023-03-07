a62fe92eece402c555a24d4c623e1248f12d246f
Ryong-E <think8867@gmail.com>
2023-03-08 00:05:24 +0900

    [#2] fix: 사용하지 않은 변수 lint오류 해결
    사용하지않은 변수 삭제

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
 
 const Main = () => {
-  const data = useLoaderData();
-
   return (
     <Box as="main">
       <h1>dkssudgktpdy</h1>
