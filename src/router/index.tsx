b48179b698dc623ee56de5c497c8d1952f450fcf
Ryong-E <think8867@gmail.com>
2023-03-08 17:41:35 +0900

    [#2] feature: main페이지 구현
    파일명 Main => TravleListPage 변경
    파일명 변경으로 router element 수정
    mainLayout component 생성
    loader를 이용하여 여행 상품 정보 전달하고 받아야할 컴포넌트에서 useLoaderData를 사용하여 데이터를 받아옵니다
    상품 상세보기 모달 구현 chakraUI에서 제공하는 modal로 구현
    예약버튼 클릭하여 장바구니에 담을 시 최대 구매횟수에 도달하게 되면 예약버튼 disabled되도록 설정 이후에 장바구니 에서 삭제나 추가될수있어 미완성적인 부분입니다

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Root from 'pages/Root';
-import Main from 'pages/Main';
+import TravleListPage from 'pages/TravleListPage';
 import rootLoader from './loader/rootLoader';
 import { PATH_ROUTES } from 'constant';
 import mainLoader from './loader/mainLoader';
@@ -14,12 +14,12 @@ const router = createBrowserRouter([
   },
   {
     path: PATH_ROUTES.main,
-    element: <Main />,
+    element: <TravleListPage />,
     loader: mainLoader,
   },
   {
     path: PATH_ROUTES.shopBasket,
-    element: <Main />,
+    element: <TravleListPage />,
   },
 ]);
 
