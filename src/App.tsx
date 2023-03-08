3e5a058146b9e179add4673f2039b5737f63316f
hongsoom <hongsoom@kakao.com>
2023-03-08 15:23:24 +0900

    [#2] feature : react-query 설정
    react-eqery를 사용하기 위해서는 세팅이 필요하기 때문에 app.tsx에 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function App() {
+const queryclient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryclient}>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
