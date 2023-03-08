cb42728cb49b199c3791f8fe6454b4238f875657
yoosion030 <rsy5487@naver.com>
2023-03-08 19:23:39 +0900

    [#2] feature: 라우팅 예외처리 추가
    유효하지 않은 주소를 입력하였을 때에도 main으로 이동하게 하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+
 import * as P from './pages';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Route path="/" element={<Navigate to="/main" />} />
       <Route path="/main" element={<P.MainPage />} />
       <Route path="/reservations" element={<P.ReservationsPage />} />
+      <Route path="/*" element={<Navigate to="/main" />} />
     </Routes>
   );
 }
