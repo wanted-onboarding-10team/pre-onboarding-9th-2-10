1d76750e3226d9febb380402c9ca1fb64875331b
yoosion030 <rsy5487@naver.com>
2023-03-08 19:21:30 +0900

    [#2] feature: chakra provider 추가
    chakra-ui를 사용하기 위한 provider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
