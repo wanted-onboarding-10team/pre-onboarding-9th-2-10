4a4dac15df8386dc56c6550c0961e8c83fe57859
yoosion030 <rsy5487@naver.com>
2023-03-10 08:37:03 +0900

    [#3] feature: 가격 필터링 구현
    input에서 min, max 값을 받아와 필터링을 구현하였습니다.

--- src/pages/TravleListPage.tsx
+++ src/pages/TravleListPage.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Box, Grid } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Button, Flex, Grid, Input } from '@chakra-ui/react';
 import MainLayout from 'components/MainLayout';
 import TravleContent from 'components/TravleContent';
-
-import { useLoaderData } from 'react-router-dom';
 import { travleContent } from 'types';
+import { useLoaderData } from 'react-router-dom';
 
 const Main = () => {
-  const data = useLoaderData() as travleContent[];
+  const mockData = useLoaderData() as travleContent[];
+  const [data, setData] = useState<travleContent[]>(mockData);
+
+  const [min, setMin] = useState<number>(0);
+  const [max, setMax] = useState<number>(30000);
+
+  const filter = () => {
+    setData(mockData?.filter(v => v.price >= min && v.price <= max));
+  };
+
   return (
     <MainLayout>
+      <Flex>
+        <Input onChange={e => setMin(Number(e.target.value))} /> ~
+        <Input onChange={e => setMax(Number(e.target.value))} />원
+        <Button isDisabled={min >= max} onClick={filter} colorScheme='red'></Button>
+      </Flex>
       <Box as='section'>
         <Grid templateColumns='repeat(2,1fr)' gap={10}>
           {data.map(product => (
