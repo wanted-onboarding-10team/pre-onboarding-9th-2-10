cd646c16aa75d11aee0da3d81173980182d4a0e7
yoosion030 <rsy5487@naver.com>
2023-03-09 17:46:55 +0900

    [#4] style: 예약 상품 없을때 스타일링 추가
    장바구니에 추가한 상품이 없을 때 예외처리 스타일링을 추가하였습니다.

--- src/pages/ReservationsPage.tsx
+++ src/pages/ReservationsPage.tsx
@@ -1,15 +1,16 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { useBasketState } from 'components/context/BasketProvider';
 import MainLayout from 'components/MainLayout';
 import ReservationsContent from 'components/ReservationsContent';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { travleContent } from 'types';
 
 const ReservationsPage = () => {
   const baskets = useBasketState();
   const [filterBaskets, setFilterBaskets] = useState<travleContent[]>([]);
   const [totalPrice, setTotalPrice] = useState<number>(0);
-
+  const navigate = useNavigate();
   useEffect(() => {
     setFilterBaskets(
       baskets
@@ -22,15 +23,22 @@ const ReservationsPage = () => {
         })
         .sort((a, b) => a.idx - b.idx),
     );
-    setTotalPrice(baskets.map(item => item.price).reduce((prev, curr) => prev + curr, 0));
+    setTotalPrice(baskets.reduce((prev, curr) => prev + curr.price, 0));
   }, [baskets]);
 
   return (
     <MainLayout>
       {baskets.length === 0 ? (
-        <>장바구니에 담은 상품이 없습니다.</>
+        <Flex flexDir={'column'} gap='10'>
+          <Text as='b' fontSize='24'>
+            장바구니에 담은 상품이 없습니다.
+          </Text>
+          <Button colorScheme='red' onClick={() => navigate('/main')}>
+            상품 담으러 가기
+          </Button>
+        </Flex>
       ) : (
-        <>
+        <Box width='700px'>
           <Flex fontSize='24' width='100%' gap='10' marginBottom={'8'} justifyContent='flex-end'>
             <Text display={'block'} as='b'>
               총 결제액 수
@@ -39,10 +47,10 @@ const ReservationsPage = () => {
               {totalPrice.toLocaleString('ko-KR')}원
             </Text>
           </Flex>
-          {filterBaskets?.map((basket, i) => (
-            <ReservationsContent {...basket} key={i} />
+          {filterBaskets?.map(basket => (
+            <ReservationsContent {...basket} key={basket.idx} />
           ))}
-        </>
+        </Box>
       )}
     </MainLayout>
   );
