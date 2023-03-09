f2c893f03fec9716c983bef23cbce9ee601dc273
SEO <108770949+Seo0H@users.noreply.github.com>
2023-03-09 20:01:12 +0900

    [#4] feature: 장바구니 기능 구현
    품목 개수, 삭제, 구매수량 변경과
    변경에 따른 결제 금액을 보여주도록 구현

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,29 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Box, Button, Card, CardBody, CardFooter, Grid, Heading, Text } from '@chakra-ui/react';
 import MainLayout from 'components/MainLayout';
 import TravleContent from 'components/TravleContent';
 
-import { useLoaderData } from 'react-router-dom';
+import { Link, Navigate, useLoaderData } from 'react-router-dom';
 import { travleContent } from 'types';
 import ReservationsContent from 'components/reservations/ReservationsContent';
+import { useBasketState } from 'components/context/BasketProvider';
+
+// const basket = getData ? (JSON.parse(getData || '') as travleContent[]) : null;
 
 const ReservationPage = () => {
-  const data = JSON.parse(localStorage.getItem('shopping-basket') || '') as travleContent[];
+  const basket = useBasketState();
+  const [delDupleData, setDupleData] = useState<travleContent[]>([]);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  let idxCounterArr = [] as number[];
+
+  useEffect(() => {
+    // SUM TOTAL PRICE & COUNT ITEM
+    if (basket.length > 0) {
+      const maxIdx = basket.sort((a, b) => a.idx - b.idx)[0].idx;
+      idxCounterArr = new Array(maxIdx + 1).fill(0);
+
+      let price = 0;
+      basket.forEach(el => {
+        idxCounterArr[el.idx]++;
+        price += el.price;
+      });
+      setTotalPrice(price);
+
+      const delDupleDataArr = [] as travleContent[];
+      basket.forEach(el => {
+        if (!delDupleDataArr.find(val => val.idx === el.idx)) delDupleDataArr.push(el);
+      });
+      setDupleData(delDupleDataArr);
+    }
+  }, [basket]);
 
   return (
     <MainLayout>
       <Grid templateColumns='1fr 400px'>
         <Box as='section'>
+          <Link to='/main'>
+            <Button marginBottom={'3'} minW={'200px'}>
+              홈으로 돌아가기
+            </Button>
+          </Link>
           <Grid templateColumns='repeat(1,1fr)' gap={10}>
-            {data.map(product => (
-              <ReservationsContent {...product} key={product.idx} />
-            ))}
+            {basket.length === 0 ? (
+              <div>데이터가 없습니다</div>
+            ) : (
+              delDupleData.map(product => {
+                return <ReservationsContent {...product} key={product.idx} />;
+              })
+            )}
           </Grid>
         </Box>
         <Card marginLeft={'10'} maxH={'200px'} variant='outline' overflow='hidden'>
           <CardBody>
-            <Heading>결제금액</Heading>
+            <Heading size={'md'} fontSize={'2rem'}>
+              결제금액 : {totalPrice.toLocaleString('ko-KR')} 원{' '}
+            </Heading>
           </CardBody>
           <Button>결제하기</Button>
         </Card>
--- src/pages/TravleListPage.tsx
+++ src/pages/TravleListPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Box, Grid } from '@chakra-ui/react';
+import { Box, Grid, Button } from '@chakra-ui/react';
 import MainLayout from 'components/MainLayout';
 import TravleContent from 'components/TravleContent';
 
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Link } from 'react-router-dom';
 import { travleContent } from 'types';
 
 const Main = () => {
@@ -11,6 +11,11 @@ const Main = () => {
   return (
     <MainLayout>
       <Box as='section'>
+        <Link to='/reservations'>
+          <Button marginBottom={'3'} minW={'200px'}>
+            장바구니
+          </Button>
+        </Link>
         <Grid templateColumns='repeat(2,1fr)' gap={10}>
           {data.map(product => (
             <TravleContent {...product} key={product.idx} />
