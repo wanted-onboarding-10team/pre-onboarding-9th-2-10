5a1062d354ac73f4bd3769ad8f759e69ac534739
Shinpanda <duldulgo1@gmail.com>
2023-03-09 18:13:27 +0900

    [#4] feature: 메뉴바 추가하여 페이지 이동 가능하게 수정

--- src/pages/TravleListPage.tsx
+++ src/pages/TravleListPage.tsx
@@ -1,25 +1,14 @@
 import { Box, Grid } from '@chakra-ui/react';
 import MainLayout from 'components/MainLayout';
 import TravleContent from 'components/TravleContent';
-import { PATH_ROUTES } from 'constant';
 
-import { Link, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { travleContent } from 'types';
 
 const Main = () => {
   const data = useLoaderData() as travleContent[];
   return (
     <MainLayout>
-      <nav>
-        <ul>
-          <li>
-            <Link to={PATH_ROUTES.main}>Home</Link>
-          </li>
-          <li>
-            <Link to={PATH_ROUTES.shopBasket}>장바구니</Link>
-          </li>
-        </ul>
-      </nav>
       <Box as='section'>
         <Grid templateColumns='repeat(2,1fr)' gap={10}>
           {data.map(product => (
